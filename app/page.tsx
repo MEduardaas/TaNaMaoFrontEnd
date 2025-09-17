db8ac277f859cd13a66253da80b0f7fde2a30bfe
kleberson154 <kleberson55@hotmail.com>
2025-09-17 13:51:13 -0300

    add: Adicionando Produtos

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,30 @@
 import Footer from '@/components/PrinComponents/Footer'
 import NavBar from '@/components/PrinComponents/NavBar'
 import CardCategories from '@/components/subComponents/CardCategories'
+import CardProducts from '@/components/subComponents/CardProducts'
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen gap-16">
       <NavBar />
-      <CardCategories title="Categoria 1" price={9.99} />
+      <div id="categories" className="flex gap-8">
+        <CardCategories title="Móveis" />
+        <CardCategories title="Eletrônicos" />
+        <CardCategories title="Livros" />
+        <CardCategories title="Ferramentas" />
+        <CardCategories title="Serviços" />
+        <CardCategories title="Jogos" />
+        <CardCategories title="Brinquedos" />
+        <CardCategories title="Automóveis" />
+        <CardCategories title="Outros" />
+      </div>
+      <div id="products" className="flex  gap-8">
+        <CardProducts title="Categoria 1" price={9.99} />
+        <CardProducts title="Categoria 2" price={19.99} />
+        <CardProducts title="Categoria 3" price={29.99} />
+        <CardProducts title="Categoria 4" price={39.99} />
+      </div>
+
       <Footer />
     </div>
   )
---
+++ components/subComponents/CardProducts/index.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export default function CardProducts({
+  title,
+  price
+}: {
+  title: string
+  price: number
+}) {
+  return (
+    <Link
+      href={`/products/${title}`}
+      className="flex flex-col items-start gap-2 border-gray-300 border-2 p-4 rounded-lg max-w-max"
+    >
+      <Image
+        src="/images/logo.png"
+        alt="Categorias"
+        width={200}
+        height={200}
+        className="rounded-lg shadow-md"
+      />
+      <h4>{title}</h4>
+      <p className="font-bold">${price}</p>
+    </Link>
+  )
+}

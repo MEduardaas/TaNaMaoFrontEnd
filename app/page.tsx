a3a4352db0b19e61751f7cf34098fff0e54a8b58
kleberson154 <kleberson55@hotmail.com>
2025-09-17 14:10:53 -0300

    add: adicionando tipagem e os produtos

--- app/page.tsx
+++ app/page.tsx
@@ -4,29 +4,34 @@ import Footer from '@/components/PrinComponents/Footer'
 import NavBar from '@/components/PrinComponents/NavBar'
 import CardCategories from '@/components/subComponents/CardCategories'
 import CardProducts from '@/components/subComponents/CardProducts'
+import { produtosTeste } from '../db/apiTest'
+
+type Produto = {
+  id: string | number
+  titulo: string
+  categoria: string
+  preco: number
+}
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen gap-16">
+    <div className="flex flex-col h-full gap-16">
       <NavBar />
-      <div id="categories" className="flex gap-8">
+      <div id="categories" className="flex gap-8 mt-18">
         <CardCategories title="Móveis" />
         <CardCategories title="Eletrônicos" />
         <CardCategories title="Livros" />
-        <CardCategories title="Ferramentas" />
-        <CardCategories title="Serviços" />
-        <CardCategories title="Jogos" />
-        <CardCategories title="Brinquedos" />
-        <CardCategories title="Automóveis" />
-        <CardCategories title="Outros" />
       </div>
-      <div id="products" className="flex  gap-8">
-        <CardProducts title="Categoria 1" price={9.99} />
-        <CardProducts title="Categoria 2" price={19.99} />
-        <CardProducts title="Categoria 3" price={29.99} />
-        <CardProducts title="Categoria 4" price={39.99} />
+      <div id="products" className="flex  gap-8 mb-10">
+        {produtosTeste.map((produto: Produto) => (
+          <CardProducts
+            key={produto.id}
+            title={produto.titulo}
+            category={produto.categoria}
+            price={produto.preco}
+          />
+        ))}
       </div>
-
       <Footer />
     </div>
   )

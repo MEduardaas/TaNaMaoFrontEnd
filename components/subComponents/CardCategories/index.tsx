73c9faff7441b6632dd1223095743eb58b737d7c
kleberson154 <kleberson55@hotmail.com>
2025-10-20 16:03:01 -0300

    refactor: melhorando o carousel dos cards

--- components/subComponents/CardCategories/index.tsx
+++ components/subComponents/CardCategories/index.tsx
@@ -46,7 +46,7 @@ export default function CardCategories({ title }: { title: string }) {
   return (
     <Link
       href={{ pathname: '/produtos', query: { categoria: title } }}
-      className="flex flex-col items-center gap-2  p-4 rounded-lg max-w-max"
+      className="flex flex-col items-center justify-center gap-2  p-4 rounded-lg max-w-max"
     >
       <div className="bg-primary p-6 rounded-full text-white">{icon}</div>
       <h4>{title}</h4>

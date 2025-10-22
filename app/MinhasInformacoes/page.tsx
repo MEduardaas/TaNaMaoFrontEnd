148865ca517c52fcf96389066ad775ea711bad7b
dudda2130 <mariadiassan147duda@gmail.com>
2025-10-22 16:17:42 -0300

    finalizando pagina minhas informacoes

--- app/MinhasInformacoes/page.tsx
+++ app/MinhasInformacoes/page.tsx
@@ -3,8 +3,6 @@
 import Footer from '@/components/PrinComponents/Footer'
 import NavBar from '@/components/PrinComponents/NavBar'
 
-import 'glider-js/glider.min.css'
-import { useState } from 'react'
 import Input from '@/components/subComponents/Input'
 import Button from '@/components/subComponents/Button'
 
@@ -13,11 +11,11 @@ export default function Page() {
   <div className="flex flex-col h-full gap-18">
       <NavBar />
       <main className="mx-auto w-full max-w-7xl px-4 ">
-        <div className="flex flex-col md:flex-row gap-6 justify-center items-center">
+        <div className="flex flex-col  gap-6 justify-center items-center">
           <div className="md:w-4/5 grid grid-cols-1 md:grid-cols-2 gap-6">
             <section className="bg-white shadow-md rounded-lg p-6">
               <h2 className="text-xl font-semibold mb-4">Minhas Informações</h2>
-              <form className="flex flex-col gap-4  p-10">
+              <form className="flex flex-col gap-4">
                <label className="flex flex-col">
                   <label htmlFor="nome">Nome completo</label>
                   <input type="text" id="name" placeholder="Nome Completo"  className="border-2 border-gray-300 rounded-lg p-2"/>
@@ -31,7 +29,7 @@ export default function Page() {
                
                 <label className="flex flex-col">
                   <label htmlFor="cpf">CPF</label>
-                  <input type="text" id="cpf" placeholder="CPF" className="border-2 border-gray-300 rounded-lg p-2" />
+                  <input type="text" id="cpf" placeholder="000.000.000-00" className="border-2 border-gray-300 rounded-lg p-2" />
                 </label>
 
                <label className="flex flex-col">
@@ -47,7 +45,7 @@ export default function Page() {
               <form >
                  <label className="flex flex-col">
                   <label htmlFor="cep">CEP</label>
-                  <input type="text" id="cep" placeholder="00000000" className="border-2 border-gray-300 rounded-lg p-2" />
+                  <input type="text" id="cep" placeholder="000000-000" className="border-2 border-gray-300 rounded-lg p-2" />
                 </label>
 
                 <label className="flex flex-col">
@@ -73,10 +71,10 @@ export default function Page() {
 
               </form>
             </section>
-           <div className="w-full px-50 ">
+          </div>
+          <div className="pt-4 w-1/5 items-center justify-center">
                <Button>Salvar</Button>
             </div>
-          </div>
         </div>
       </main>
       <Footer />

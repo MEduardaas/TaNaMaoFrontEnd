603d9f9e85593749b7b8a4e490938031bb0a38d8
kleberson154 <kleberson55@hotmail.com>
2025-10-26 11:00:46 -0300

    feat: transformando a pagina negado em loading

---
+++ app/Carregando/page.tsx
@@ -0,0 +1,20 @@
+import Footer from '@/components/PrinComponents/Footer'
+import NavBar from '@/components/PrinComponents/NavBar'
+import Button from '@/components/subComponents/Button'
+import LinkNavigation from '@/components/subComponents/LinkNavigation'
+import Image from 'next/image'
+import React from 'react'
+
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen">
+      <NavBar />
+      <div className="mt-12 flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold text-indigo-400 my-15">
+          Carregando ...
+        </h1>
+      </div>
+      <Footer />
+    </main>
+  )
+}

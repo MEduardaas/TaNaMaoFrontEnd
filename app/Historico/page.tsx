b92d81cb284bfe43b02717c909c1fb4f9cdbba8e
diasMariya <mariadiassan147duda@gmail.com>
2025-10-27 19:56:00 -0300

    ajustando pagina de historico

--- app/Historico/page.tsx
+++ app/Historico/page.tsx
@@ -7,23 +7,13 @@ export default function Page() {
   const [abaAtiva, setAbaAtiva] = useState("Compras");
 
   const produtos: Record<string, { id: number; nome: string; preco: string; imagem: string }[]> = {
-    Compras: [
-      { id: 1, nome: "Sofá retrátil", preco: "R$ 1700,00", imagem: "" },
-      { id: 2, nome: "Sofá de canto", preco: "R$ 2800,00", imagem: "" },
-      { id: 3, nome: "Sofá modular", preco: "R$ 4500,00", imagem: "" },
-    ],
-    Vendas: [
-      { id: 4, nome: "Cadeira de escritório", preco: "R$ 600,00", imagem: "" },
-    ],
-    Aluguéis: [
-      { id: 5, nome: "Mesa de jantar", preco: "R$ 200,00 / mês", imagem: "" },
-    ],
+    Compras: [],
+    Vendas: [],
+    Aluguéis: [],
   };
   return (
        <div className="flex flex-col h-full gap-16">
           <NavBar />
-          
-        
     <div className="flex flex-col ml-10 mt-10 font-sans">
     <h1 className="text-4xl p-4">Histórico</h1>
       <div className="flex gap-6 border-b border-gray-200 mb-4 w-1/2">
@@ -44,11 +34,6 @@ export default function Page() {
       <div className="p-8 shadow-md rounded-2xl w-64 border bg-white">
         {produtos[abaAtiva].map((p) => (
           <div key={p.id} className="flex items-center mb-3 gap-3">
-           {/*  <img
-              src={p.imagem}
-              alt={p.nome}
-              className="w-12 h-12 rounded-md object-cover"
-            /> */}
             <div>
               <p className="font-semibold text-gray-800">{p.nome}</p>
               <p className="text-gray-500 text-sm">{p.preco}</p>
